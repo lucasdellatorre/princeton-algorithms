d6c1955ce8159a2b7d264cc68e0a5d12080dca13
lucasdellatorre <lucasdellatorre222@gmail.com>
2024-03-02 22:50:43 -0300

    almost finished

--- unionfind/practice/montercarlosim/main.go
+++ unionfind/practice/montercarlosim/main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"fmt"
-	"math"
+	"math/rand"
 )
 
 // Percolation. We model the system as an n-by-n grid of sites. Each site is either blocked or open; open sites are initially empty. A full site is an open site that can be connected to an open site in the top row via a chain of neighboring (left, right, up, down) open sites. If there is a full site in the bottom row, then we say that the system percolates.
@@ -17,23 +17,94 @@ type UnionFind struct {
 	id   []int
 	sz   []int
 	grid []bool
+	n    int
 }
 
 func main() {
+	// New(rand.NewSource(time.Now().UnixNano()))
 	n := 4
-	u := newUnionFind(n*n + 2) // + 2 top and bottom virtual
+	u := newUnionFind(n)
 
-	u.printUf()
-	u.percolates()
+	for i := 0; i < n; i++ {
+		u.union(i, n*n)
+	}
+
+	for i := n * (n - 1); i < n*n; i++ {
+		u.union(i, n*n+1)
+	}
+
+	randomNumbers := generateUniqueRandomNumbers(n * n)
+
+	fmt.Println(randomNumbers)
+
+	for i := 0; i < n*n; i++ {
+		u.Open(randomNumbers[i])
+		u.printUf()
+	}
 }
 
-func Union() {
+func generateUniqueRandomNumbers(n int) []int {
+	randomNumbers := make([]int, n)
+	usedNumbers := make(map[int]bool)
 
+	for i := 0; i < n; i++ {
+		var num int
+		for {
+			num = rand.Intn(n)
+			if !usedNumbers[num] {
+				break
+			}
+		}
+		randomNumbers[i] = num
+		usedNumbers[num] = true
+	}
+
+	return randomNumbers
+}
+
+func (u *UnionFind) isOpen(i int) bool {
+	return !u.grid[i]
+}
+
+func (u *UnionFind) Open(i int) {
+	u.grid[i] = false
+	if i-1 > 0 && u.isOpen(i-1) { // left
+		u.union(i, i-1)
+	}
+	if i+1 < len(u.grid) && u.isOpen(i+1) { // right
+		u.union(i, i+1)
+	}
+	if i+u.n < len(u.grid) && u.isOpen(i+u.n) { // top
+		u.union(i, i+1)
+	}
+	if i-u.n > 0 && u.isOpen(i-u.n) { // left
+		u.union(i, i-1)
+	}
+	// u.union(i, i-u.n) // top
+	// u.union(i, i+u.n) // bottom
+}
+
+func (u *UnionFind) union(p int, q int) {
+	i := u.root(p)
+	j := u.root(q)
+
+	if i == j {
+		return
+	}
+
+	if u.sz[i] < u.sz[j] {
+		u.id[i] = j
+		u.sz[j] += u.sz[i]
+
+	} else {
+		u.id[j] = i
+		u.sz[i] += u.sz[j]
+
+	}
 }
 
 func (u *UnionFind) root(i int) int {
 	for i != u.id[i] {
-		fmt.Println(u.id)
 		u.id[i] = u.id[u.id[i]] // path compression
 		i = u.id[i]
 	}
@@ -45,53 +116,78 @@ func (u *UnionFind) connection(p, q int) bool {
 }
 
 func (u *UnionFind) percolates() bool {
-	virtualTop := u.id[len(u.id)-1]
-	virtualBottom := u.id[len(u.id)-2]
-	return u.connection(virtualBottom, virtualTop)
+	return u.connection(4*4, 4*4+1)
 }
 
 func newUnionFind(n int) *UnionFind {
+	id := make([]int, n*n+2)
+	sz := make([]int, n*n+2)
+	grid := make([]bool, n*n)
+
+	initializeIdMatrix(id)
+	initializeSizeMatrix(sz)
+	initializeGridMatrix(grid)
+
 	uf := &UnionFind{
-		id:   make([]int, n),
-		sz:   make([]int, n),
-		grid: make([]bool, n),
+		id:   id,
+		sz:   sz,
+		grid: grid,
+		n:    n,
 	}
-
-	for i := range uf.id {
-		uf.id[i] = i
-		uf.sz[i] = 1
-		uf.grid[i] = true
-	}
-
 	return uf
 }
 
-func printIntegerMatrix(matrix []int) {
-	n := int(math.Sqrt(float64(len(matrix)))) // remove this for god sake
+func initializeIdMatrix(matrix []int) {
 	for i := range matrix {
-		if i > 0 && i%n == 0 {
-			fmt.Println()
-		}
-		fmt.Printf(" %d ", matrix[i])
+		matrix[i] = i
 	}
 }
 
-func printBooleanMatrix(matrix []bool) {
-	n := int(math.Sqrt(float64(len(matrix)))) // remove this for god sake
+func initializeSizeMatrix(matrix []int) {
 	for i := range matrix {
-		if i > 0 && i%n == 0 {
+		matrix[i] = 1
+	}
+}
+
+func initializeGridMatrix(matrix []bool) {
+	for i := range matrix {
+		matrix[i] = true
+	}
+}
+
+func (u *UnionFind) printIdMatrix() {
+	for i := 0; i < u.n*u.n; i++ {
+		if i > 0 && i%u.n == 0 {
 			fmt.Println()
 		}
-		fmt.Printf(" %t ", matrix[i])
+		fmt.Printf(" %d ", u.id[i])
+	}
+}
+
+func (u *UnionFind) printSizeMatrix() {
+	for i := 0; i < u.n*u.n; i++ {
+		if i > 0 && i%u.n == 0 {
+			fmt.Println()
+		}
+		fmt.Printf(" %d ", u.sz[i])
+	}
+}
+
+func (u *UnionFind) printGridMatrix() {
+	for i := 0; i < u.n*u.n; i++ {
+		if i > 0 && i%u.n == 0 {
+			fmt.Println()
+		}
+		fmt.Printf(" %t ", u.grid[i])
 	}
 }
 
 func (u *UnionFind) printUf() {
-	printIntegerMatrix(u.id)
+	u.printIdMatrix()
 	fmt.Println()
-	printIntegerMatrix(u.sz)
+	u.printSizeMatrix()
 	fmt.Println()
-	printBooleanMatrix(u.grid)
+	u.printGridMatrix()
 	fmt.Println()
-	u.percolates()
+	fmt.Println("Percolates?", u.percolates())
 }
